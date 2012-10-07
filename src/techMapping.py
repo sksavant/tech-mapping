ba7bcec36369d2c791a33135d0437091cfbcc4a1
Savant Krishna <savant.2020@gmail.com>
2012-10-07 15:41:10 +0530

    Added README, touched file, document written by Anurag

---
+++ src/techMapping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+class TechMapping:
+    #define functions
+    def __init__(self):
+        #constructor
+
+
+if __name__=="__main__":
+    #do something
+    tm = TechMapping()
+    print tm.finalAllocation()
