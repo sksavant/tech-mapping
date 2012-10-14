0d79c6ad9417b8342ec958238c305d95029e4980
Savant Krishna <savant.2020@gmail.com>
2012-10-14 16:45:15 +0530

    loading a graphml file and showing it

--- src/techMapping.py
+++ src/techMapping.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python
 import sys
-import graphtool
+from graph_tool.all import *
 class TechMapping:
     #define functions
     def __init__(self):
         #constructor
         #initialise the graph from the graphml file specified in the argument.
-        self._graph_input_file = sys.argv[1] #take the input graph file from the terminal arguments
-        self._g = load_graph(_graph_input_file) #load the graphml file as a graph object in graph-tool
+        try:
+            self._graph_input_file = sys.argv[1] #take the input graph file from the terminal arguments
+            try:
+                self._g = load_graph(self._graph_input_file) #load the graphml file as a graph object in graph-tool
+                print "intialised well"
+            except ValueError:
+                print "Cannot read the file given:",self._graph_input_file
+        except IndexError:
+            print "ERROR: Graphml file is not specified"
+             
+        
+    
+    def finalAllocation(self):
+        #To print the final allocation of the logic elements
+        print "The final gate implementation of the logic circuit is :"
 
 if __name__=="__main__":
     #do something
     tm = TechMapping()
-    print tm.finalAllocation()
+    graph_draw(tm._g, vertex_text=tm._g.vertex_index, vertex_font_size=12, output_size=(200,200), output="viewgraph.pdf")
+    tm.finalAllocation()
+    print "ending"
