87020b9970c2ad635eb80dc21eace5c81cc65850
Savant Krishna <savant.2020@gmail.com>
2012-10-14 15:25:31 +0530

    started working : specified input blocks

--- src/techMapping.py
+++ src/techMapping.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-
+import sys
+import graphtool
 class TechMapping:
     #define functions
     def __init__(self):
         #constructor
-
+        #initialise the graph from the graphml file specified in the argument.
+        self._graph_input_file = sys.argv[1] #take the input graph file from the terminal arguments
+        self._g = load_graph(_graph_input_file) #load the graphml file as a graph object in graph-tool
 
 if __name__=="__main__":
     #do something
