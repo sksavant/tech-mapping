056f7372259097cfcb477f45561dc34277a5fe05
Savant Krishna <savant.2020@gmail.com>
2012-11-15 20:53:04 +0530

    3nand working

--- src/techMapping.py
+++ src/techMapping.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+from operator import itemgetter
 #import os
 from graph_tool.all import *
 class TechMapping:
@@ -56,19 +57,19 @@ class TechMapping:
             fan_in_vertices.append(e.target())
         p=len(fan_in_vertices)
         while p>1:
-           if(p%2==1):
-              self.two_input_and_gate(fan_in_vertices,p/2,-1,temp_vertices)
-              fan_in_vertices=fan_in_vertices[:-2]
-              fan_in_vertices.append(temp_vertices[0])
-              temp_vertices=[]
-           q=len(fan_in_vertices)/2
-           for x in range (0,q):
+            if(p%2==1):
+                self.two_input_and_gate(fan_in_vertices,p/2,-1,temp_vertices)
+                fan_in_vertices=fan_in_vertices[:-2]
+                fan_in_vertices.append(temp_vertices[0])
+                temp_vertices=[]
+            q=len(fan_in_vertices)/2
+            for x in range (0,q):
                 self.two_input_and_gate(fan_in_vertices,x,1,temp_vertices)
-           fan_in_vertices=temp_vertices
-           temp_vertices=[]
-           p=len(fan_in_vertices)
+            fan_in_vertices=temp_vertices
+            temp_vertices=[]
+            p=len(fan_in_vertices)
         for e in vertex.in_edges():
-           g.add_edge(e.source(),fan_in_vertices[0] )
+            g.add_edge(e.source(),fan_in_vertices[0] )
         g.clear_vertex(vertex)
         g.remove_vertex(vertex)
 
@@ -80,17 +81,17 @@ class TechMapping:
             fan_in_vertices.append(e.target())
         p=len(fan_in_vertices)
         while p>1:
-           if(p%2==1):
-              self.two_input_or_gate(fan_in_vertices,p/2,-1,temp_vertices)
-              fan_in_vertices=fan_in_vertices[:-2]
-              fan_in_vertices.append(temp_vertices[0])
-              temp_vertices=[]
-           q=len(fan_in_vertices)/2
-           for x in range (0,q):
+            if(p%2==1):
+                self.two_input_or_gate(fan_in_vertices,p/2,-1,temp_vertices)
+                fan_in_vertices=fan_in_vertices[:-2]
+                fan_in_vertices.append(temp_vertices[0])
+                temp_vertices=[]
+            q=len(fan_in_vertices)/2
+            for x in range (0,q):
                 self.two_input_or_gate(fan_in_vertices,x,1,temp_vertices)
-           fan_in_vertices=temp_vertices
-           temp_vertices=[]
-           p=len(fan_in_vertices)
+            fan_in_vertices=temp_vertices
+            temp_vertices=[]
+            p=len(fan_in_vertices)
         for e in vertex.in_edges():
             g.add_edge(e.source(),fan_in_vertices[0])
         g.clear_vertex(vertex)
@@ -101,15 +102,15 @@ class TechMapping:
         k=1
         while(k==1):
             k=0
-        for vertex in g.vertices():
-                    if g.vertex_properties["name"][vertex]=="&":
-                        self.balanced_n_input_and_gate(vertex)
-                        k=1
-                        break
-                    elif g.vertex_properties["name"][vertex]=="|":
-                        self.balanced_n_input_or_gate(vertex)
-                        k=1
-                        break
+            for vertex in g.vertices():
+                if g.vertex_properties["name"][vertex]=="&":
+                    self.balanced_n_input_and_gate(vertex)
+                    k=1
+                    break
+                elif g.vertex_properties["name"][vertex]=="|":
+                    self.balanced_n_input_or_gate(vertex)
+                    k=1
+                    break
 
     def not_redundancy_removal(self):
         g= self._graph_original
@@ -161,14 +162,14 @@ class TechMapping:
             g.clear_vertex(e)
             #print "newtrv",newtrv
         # The great loop is here...
-            k=1
-            while(k==1):
-                k=0
+        k=1
+        while(k==1):
+            k=0
             for vertex in g.vertices():
-                        if g.vertex_properties["name"][vertex]=="n!":
-                            g.remove_vertex(vertex)
-                            k=1
-                            break
+                if g.vertex_properties["name"][vertex]=="n!":
+                    g.remove_vertex(vertex)
+                    k=1
+                    break
 
     def do_dfs(self): #dFS on graph to number the numbers... a number on the node denotes when it should be fucked.
         g = self._graph_original
@@ -213,8 +214,25 @@ class TechMapping:
 
     def find_optimal_pattern(self):
         #For each vertex in the sorted_list, arise the function optimal_pattern(v) to find the optimal pattern (obviously!)
+        g = self._graph_original
+        vprop_cost = g.new_vertex_property("int")
+        vprop_gate = g.new_vertex_property("string")
+        vprop_edge = g.new_vertex_property("int")
+        g.vertex_properties["cost"] = vprop_cost
+        g.vertex_properties["gate"] = vprop_gate
+        g.vertex_properties["edge"] = vprop_edge
+        vpc = g.vertex_properties["cost"]
+        vpg = g.vertex_properties["gate"]
+        vpe = g.vertex_properties["edge"]
+        vpn = g.vertex_properties["name"]
+        for v in g.vertices():
+            if v.out_degree()==0:
+                [vpc[v],vpg[v],vpe[v]]=[0,vpn[v],-1]
+            else:
+                [vpc[v],vpg[v],vpe[v]]=[0,"",-1]
         for v in self.sorted_list:
-            self.optimal_pattern(v)
+            if vpn[v]=='!' or vpn[v]=='NAND':
+                self.optimal_pattern(v)
 
     def optimal_pattern(self,v):
         #Went to vertex v. It is guaranteed that all the inputs to this vertex were already called.
@@ -222,13 +240,89 @@ class TechMapping:
         g = self._graph_original
         vpn = g.vertex_properties["name"]
         vpl = g.vertex_properties["level"]
-        for l in self.library_graphs:
-            gl = l._graph_original
-            vpnl = gl.vertex_properties["name"]
-            vpll = gl.vertex_properties["level"]
-            if vpn[v] == vpnl[l.sources[0]]: #the source vertex of the library graph
-            #Got a top level match wtf to do now?
-                a=2
+        vpc = g.vertex_properties["cost"]
+        vpg = g.vertex_properties["gate"]
+        vpe = g.vertex_properties["edge"]
+        #for l in self.library_graphs:
+        #    gl = l._graph_original
+        #    vpnl = gl.vertex_properties["name"]
+        #    vpll = gl.vertex_properties["level"]
+        #    if vpn[v] == vpnl[l.sources[0]]: #the source vertex of the library graph
+        #    #Got a top level match wtf to do now?
+        #   
+        costs=[]
+        costs.append(self.find_3in_nand(v))
+        costs.append(self.find_2in_nand(v))
+        costs.append(self.find_not(v))
+        for x in costs:
+            if x[0]<0:
+                costs.pop(costs.index(x))
+        costs = sorted(costs,key=itemgetter(0))
+        i=0
+        print costs
+        while(True):
+            try:
+                if (costs[i][0]>0):
+                    [vpc[v],vpg[v],vpe[v]] = costs[i]
+                    break
+                i=i+1
+            except IndexError:
+                pass
+
+    def find_3in_nand(self,v):
+        g = self._graph_original
+        vpn = g.vertex_properties["name"]
+        vpc = g.vertex_properties["cost"]
+        vpg = g.vertex_properties["gate"]
+        vpe = g.vertex_properties["edge"]
+        if vpn[v]=='NAND':
+            for nv in v.out_neighbours():
+                if vpn[nv]=='!':
+                    for nnv in nv.out_neighbours():
+                        if vpn[nnv]=='NAND':
+                            cost = 0
+                            for nnnv in nnv.out_neighbours():
+                                cost = vpc[nnnv]+cost
+                                print cost
+                            for xv in v.out_neighbours():
+                                if xv!=nv:
+                                    print xv,nv
+                                    cost = cost + vpc[xv]
+                                    print cost
+                            edge =  g.edge(v,nv)
+                            return [cost+3,'3N',int(g.edge_index[edge])]
+                            break
+        return [None,"",-1]
+
+    def find_2in_nand(self,v):
+        g = self._graph_original
+        vpn = g.vertex_properties["name"]
+        vpc = g.vertex_properties["cost"]
+        vpg = g.vertex_properties["gate"]
+        vpe = g.vertex_properties["edge"]
+        if vpn[v] == 'NAND':
+            cost = 0
+            for nv in v.out_neighbours():
+                cost = vpc[nv]+cost
+            edge = None
+            return [cost+2,'2N',-1]
+        else:
+            return [-1,"",-1]
+
+    def find_not(self,v):
+        g = self._graph_original
+        vpn = g.vertex_properties["name"]
+        vpc = g.vertex_properties["cost"]
+        vpg = g.vertex_properties["gate"]
+        vpe = g.vertex_properties["edge"]
+        if vpn[v] == '!':
+            cost = 0
+            for nv in v.out_neighbours():
+                cost = vpc[nv]+cost
+            edge = None
+            return [cost+1,'!',-1]
+        else:
+            return [-1,"",-1]
 
     def load_library_functions(self):
         self.library_graphs=[]
@@ -246,7 +340,6 @@ class TechMapping:
          #To print the final allocation of the logic elements
          print "The final gate implementation of the logic circuit is :"
 
-
     def find_sources(self):
         g = self._graph_original
         vp = g.vertex_properties["name"]
@@ -258,14 +351,15 @@ class TechMapping:
             print vp[v]
         return
 
+
 if __name__=="__main__":
     #do something
     tm = TechMapping()
     tm.ConvertInputToBaseGates()
     tm.not_redundancy_removal()
     tm.do_dfs()
-    tm.load_library_functions()
+    #tm.load_library_functions()
     tm.find_optimal_pattern()
-    graph_draw(tm._graph_original, vertex_fill_color="#729fcf", vertex_text=tm._graph_original.vertex_properties["name"], vertex_font_size=10, output_size=(2000,2000))
+    graph_draw(tm._graph_original, vertex_fill_color="#729fcf", vertex_text=tm._graph_original.vertex_properties["edge"], vertex_font_size=10, output_size=(2000,2000))
     tm.finalAllocation()
     print "ending"
